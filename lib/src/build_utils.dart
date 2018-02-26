1b16a4d5a2609d959b8ebefd4da943861c0829e7
Vittorio Ballestra <vittorio.ballestra@gmail.com>
2018-02-26 23:39:19 +0100

    some utils

---
+++ lib/src/build_utils.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:build_runner/build_runner.dart' as builder;
+
+import 'package:dart2ts/src/code_generator2.dart';
+
+npm([List<String> args = const ['run', 'build']]) async {
+  Process npm = await Process.start('npm', args);
+  stdout.addStream(npm.stdout);
+  stderr.addStream(npm.stderr);
+  int exitCode = await npm.exitCode;
+  if (exitCode != 0) {
+    throw "Build error";
+  }
+}
+
+enum Mode { APPLICATION, LIBRARY }
+
+class BuildException {
+  builder.BuildResult _result;
+
+  BuildException(this._result);
+
+  builder.BuildResult get result => _result;
+
+  toString() => "Build Exception ${_result.exception}";
+}
+
+Future<builder.BuildResult> tsbuild({String basePath: '.', bool clean: true, Mode mode: Mode.LIBRARY}) async {
+  if (clean) {
+    Directory dir = new Directory(path.join(basePath, '.dart_tool'));
+    if (dir.existsSync()) dir.deleteSync(recursive: true);
+  }
+
+  builder.PackageGraph packageGraph = new builder.PackageGraph.forPath(basePath);
+
+  builder.BuildResult res = await builder.build([
+    new builder.BuildAction(
+        new Dart2TsBuilder(new Config(modulePrefix: mode == Mode.LIBRARY ? 'node_modules' : '../node_modules')),
+        packageGraph.root.name,
+        inputs: ['lib/**.dart']),
+  ], deleteFilesByDefault: true, packageGraph: packageGraph);
+
+  if (res.status != builder.BuildStatus.success) {
+    throw new BuildException(res);
+  }
+
+  return res;
+}
+
+Future fixDependencyPath(String dist) async {
+  // Replace "node_modules" with relative url
+
+  RegExp re = new RegExp("import([^\"]*)\"node_modules/(.*)");
+  await for (FileSystemEntity f in new Directory(dist).list(recursive: true)) {
+    if (f is File) {
+      List<String> lines = await f.readAsLines();
+      IOSink sink = f.openWrite();
+      lines.map((l) {
+        Match m = re.matchAsPrefix(l);
+        if (m != null) {
+          l = "import${m[1]}\"../../${m[2]}";
+        }
+
+        return l;
+      }).forEach((l) => sink.writeln(l));
+
+      await sink.close();
+    }
+  }
+}
+
+Future copyAssets(String dist, {String basePath: '.'}) async {
+  // Copy assets
+  String srcPath = path.join(basePath, 'lib');
+
+  await for (FileSystemEntity f in new Directory(srcPath).list(recursive: true)) {
+    if (f is File && !f.path.endsWith('.dart') && !f.path.endsWith('.ts')) {
+      File d = new File(path.join(dist, path.relative(f.path, from: srcPath)));
+      if (!d.parent.existsSync()) {
+        await d.parent.create(recursive: true);
+      }
+      await f.copy(d.path);
+    }
+  }
+}
+
+Future finishLibrary({String basePath: '.'}) async {
+  File tsconfigFile = new File(path.join(basePath, 'tsconfig.json'));
+  var tsconfig = JSON.decode(tsconfigFile.readAsStringSync());
+  String dist = path.joinAll([basePath, tsconfig['compilerOptions']['outDir'], 'lib']);
+  await fixDependencyPath(dist);
+  await copyAssets(dist);
+}
